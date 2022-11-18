89935e9a3dd182096f190c73b1221256c9f068f8
AlexArmor <lexlutermotion@gmail.com>
2022-11-19 00:23:07 +0200

    corrected when mistakes fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce'
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
@@ -10,49 +10,55 @@ const oneCountry = document.querySelector('.country-info');
 
 const input = document.querySelector('#search-box');
 
-const countrySearch = (event) => {
-    const prettyInputValue = event.target.value.trim();
-    if (!prettyInputValue) {
-        counties.innerHTML = "";
-        oneCountry.innerHTML = "";
+const countrySearch = event => {
+  const prettyInputValue = event.target.value.trim();
+  if (!prettyInputValue) {
+    counties.innerHTML = '';
+    oneCountry.innerHTML = '';
+    return;
+  }
+  fetchCountries(prettyInputValue)
+    .then(data => {
+      if (data.length > 10) {
+        counties.innerHTML = '';
+        oneCountry.innerHTML = '';
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
         return;
-    }
-    fetchCountries(prettyInputValue)
-        .then(data => {
-            if (data.length > 10) {
-                counties.innerHTML = "";
-                oneCountry.innerHTML = "";
-                Notify.info('Too many matches found. Please enter a more specific name.');
-                return;
-            } else if (data.length > 1 && data.length <= 10) {
-                const markup = data.map(({ name, flags }) => {
-                    return `<li class="countries__item">
+      } else if (data.length > 1 && data.length <= 10) {
+        const markup = data
+          .map(({ name, flags }) => {
+            return `<li class="countries__item">
                     <img class="flag" src="${flags.svg}">
                     <p>${name.official}</p>
-                    </li>`
-                }).join('');
-                counties.innerHTML = markup;
-                oneCountry.innerHTML = "";
-            } else {
-                const element = data[0];
-                const language = Object.values(element.languages).join(', ');
-                const capital = element.capital.join(', ')
-                const markup = `<div class="country-title">
+                    </li>`;
+          })
+          .join('');
+        counties.innerHTML = markup;
+        oneCountry.innerHTML = '';
+      } else {
+        const element = data[0];
+        const language = Object.values(element.languages).join(', ');
+        const capital = element.capital.join(', ');
+        const markup = `<div class="country-title">
                 <img class="flag" src="${element.flags.svg}">
                 <h1 class="country-name">${element.name.official}</h>
                 </div>
                 <p class="section-name">Capital: <span>${capital}</span></p>
                 <p class="section-name">Population: <span>${element.population}</span></p>
                 <p class="section-name">Languages: <span>${language}</span></p>`;
-                oneCountry.innerHTML = markup;
-                counties.innerHTML = "";
-                console.dir(element);
-            }
-        })
-        .catch(err => {
-            Notify.failure("Oops, there is no country with that name");
-            return;
-        });
+        oneCountry.innerHTML = markup;
+        counties.innerHTML = '';
+        console.dir(element);
+      }
+    })
+    .catch(err => {
+      counties.innerHTML = '';
+      oneCountry.innerHTML = '';
+      Notify.failure('Oops, there is no country with that name');
+      return;
+    });
 };
 
-input.addEventListener('input', debounce(countrySearch, DEBOUNCE_DELAY)); 
+input.addEventListener('input', debounce(countrySearch, DEBOUNCE_DELAY));
